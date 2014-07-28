d605196f6bb2b9e8b4ad770f246ceb057c8c3aa7
liuyenting <windows.linux.mac@gmail.com>
2014-07-29 00:03:09 +0800

    Update Help contents.

--- Program Editor/HelpForm.Designer.cs
+++ Program Editor/HelpForm.Designer.cs
@@ -33,8 +33,11 @@ namespace Program_Editor
 			this.pictureBox1 = new System.Windows.Forms.PictureBox();
 			this.pictureBox2 = new System.Windows.Forms.PictureBox();
 			this.btnOK = new System.Windows.Forms.Button();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
 			( (System.ComponentModel.ISupportInitialize)( this.pictureBox1 ) ).BeginInit();
 			( (System.ComponentModel.ISupportInitialize)( this.pictureBox2 ) ).BeginInit();
+			( (System.ComponentModel.ISupportInitialize)( this.pictureBox3 ) ).BeginInit();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -75,7 +78,7 @@ namespace Program_Editor
 			// 
 			// btnOK
 			// 
-			this.btnOK.Location = new System.Drawing.Point( 132, 125 );
+			this.btnOK.Location = new System.Drawing.Point( 124, 175 );
 			this.btnOK.Name = "btnOK";
 			this.btnOK.Size = new System.Drawing.Size( 75, 23 );
 			this.btnOK.TabIndex = 4;
@@ -83,12 +86,32 @@ namespace Program_Editor
 			this.btnOK.UseVisualStyleBackColor = true;
 			this.btnOK.Click += new System.EventHandler( this.btnOK_Click );
 			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = global::Program_Editor.Properties.Resources.revert;
+			this.pictureBox3.Location = new System.Drawing.Point( 65, 119 );
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size( 35, 36 );
+			this.pictureBox3.TabIndex = 6;
+			this.pictureBox3.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point( 19, 130 );
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size( 297, 13 );
+			this.label3.TabIndex = 5;
+			this.label3.Text = "3. Click                (Revert Conversion) to begin the reversions.";
+			// 
 			// HelpForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size( 338, 165 );
+			this.ClientSize = new System.Drawing.Size( 338, 225 );
 			this.ControlBox = false;
+			this.Controls.Add( this.pictureBox3 );
+			this.Controls.Add( this.label3 );
 			this.Controls.Add( this.btnOK );
 			this.Controls.Add( this.pictureBox2 );
 			this.Controls.Add( this.pictureBox1 );
@@ -101,6 +124,7 @@ namespace Program_Editor
 			this.Text = "Help";
 			( (System.ComponentModel.ISupportInitialize)( this.pictureBox1 ) ).EndInit();
 			( (System.ComponentModel.ISupportInitialize)( this.pictureBox2 ) ).EndInit();
+			( (System.ComponentModel.ISupportInitialize)( this.pictureBox3 ) ).EndInit();
 			this.ResumeLayout( false );
 			this.PerformLayout();
 
@@ -113,5 +137,7 @@ namespace Program_Editor
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.PictureBox pictureBox2;
 		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.Label label3;
 	}
 }
